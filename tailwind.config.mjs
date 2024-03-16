8b55be4f38aeef0e25cba9c7fc5ab60a2f95d7a4
Fernando Vásquez <f.vasquez@softec.cloud>
2024-03-16 19:18:25 +0100

    Adding MainSection and PlayerBar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,11 @@ export default {
 	theme: {
 		extend: {
 			colors: {
+				'accent': '#1ED760',
 				'elevated': '#121212',
 				'elevated-2': '#222222',
+				'elevated-3': '#292929',
+				'elevated-4': '#727272',
 			},
 		},
 	},
