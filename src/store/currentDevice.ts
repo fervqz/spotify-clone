d91f34969c599bc399f0025073af15e4f56651cf
Fernando Vásquez <f.vasquez@softec.cloud>
2024-03-18 00:53:08 +0100

    Making current device banner hideable

---
+++ src/store/currentDevice.ts
@@ -0,0 +1,30 @@
+import { atom } from 'nanostores'
+
+interface Device {
+  showBanner: boolean;
+  type: 'mobile' | 'desktop' | 'tablet';
+  name: string;
+}
+
+export const $currentDevice = atom<Device>({
+  showBanner: true,
+  type: 'desktop',
+  name: "Jon Snow's Macbook Pro",
+})
+
+export function setCurrentDevice(newDevice: Device) {
+  $currentDevice.set(newDevice);
+}
+
+export function getCurrentDevice() {
+  return $currentDevice.get();
+}
+
+export function toggleCurrentDevice() {
+  console.log('toggle');
+  const newValue = !$currentDevice.get().showBanner;
+  $currentDevice.set({
+    ...$currentDevice.get(),
+    showBanner: newValue,
+  });
+}
