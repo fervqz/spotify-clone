eb61c7b49d03814ae98866d053e555e64a22dc86
Fernando Vásquez <f.vasquez@softec.cloud>
2024-03-18 20:45:36 +0100

    Fixing main app layout and adding background colro transition to main section header

---
+++ src/constants/libraries.ts
@@ -0,0 +1,59 @@
+const LIBRARIES = [
+    {
+      img: "",
+      title: "Liked Songs",
+      subtitle: "📌 Playlist • 1,005 songs",
+    },
+    {
+      img: "",
+      title: "Metallica",
+      subtitle: "Album • Metallica",
+    },
+    {
+      img: "",
+      title: "Your Top Songs 2023",
+      subtitle: "Playlist • Spotify",
+    },
+    {
+      img: "",
+      title: "Back in Black",
+      subtitle: "Album • AC/CD",
+    },
+    {
+      img: "",
+      title: "The Book of Souls",
+      subtitle: "Album • Iron Maiden",
+    },
+    {
+      img: "",
+      title: "Rock Party",
+      subtitle: "Playlist • Spotify",
+    },
+    {
+      img: "",
+      title: "This Is Michael jackson",
+      subtitle: "Playlist • Spotify",
+    },
+    {
+      img: "",
+      title: "80s Rock Anthems",
+      subtitle: "Playlist • Spotify",
+    },
+    {
+      img: "",
+      title: "Your Top Songs 2022",
+      subtitle: "Playlist • Spotify",
+    },
+    {
+      img: "",
+      title: "Your Top Songs 2021",
+      subtitle: "Playlist • Spotify",
+    },
+    {
+      img: "",
+      title: "Your Top Songs 2022",
+      subtitle: "Playlist • Spotify",
+    },
+  ];
+
+  export default LIBRARIES;
