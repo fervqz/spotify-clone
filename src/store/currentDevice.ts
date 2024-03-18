5bd552f9b1f69c6c0e5027d68de6c3b66d2afffb
Fernando Vásquez <f.vasquez@softec.cloud>
2024-03-18 21:15:18 +0100

    Fixing more ts errors

--- src/store/currentDevice.ts
+++ src/store/currentDevice.ts
@@ -1,18 +1,18 @@
 import { atom } from 'nanostores'
 
-interface Device {
+export interface CurrentDevice {
   showBanner: boolean;
   type: 'mobile' | 'desktop' | 'tablet';
   name: string;
 }
 
-export const $currentDevice = atom<Device>({
+export const $currentDevice = atom<CurrentDevice>({
   showBanner: true,
   type: 'desktop',
   name: "Jon Snow's Macbook Pro",
 })
 
-export function setCurrentDevice(newDevice: Device) {
+export function setCurrentDevice(newDevice: CurrentDevice) {
   $currentDevice.set(newDevice);
 }
 
